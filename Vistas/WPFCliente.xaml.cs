35073e02cd0b1c14981cedd3da751c1e3e1aa742
andi <andiipuckett@gmail.com>
2020-09-10 01:39:53 -0300

    Alta Cliente, alta pelicula

--- Vistas/WPFCliente.xaml.cs
+++ Vistas/WPFCliente.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using ClasesBase;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Vistas
 {
@@ -11,5 +15,94 @@ namespace Vistas
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Camptura de datos 
+        /// </summary>
+        /// <param name="cliente"></param>
+        private void capturarValores(Cliente cliente)
+        {
+            cliente.Cli_DNI = Int32.Parse(txtDni.Text);
+            cliente.Cli_Apellido = txtApellido.Text;
+            cliente.Cli_Nombre = txtNombre.Text;
+            cliente.Cli_Telefono = txtTelefono.Text;
+            cliente.Cli_Email = txtEmail.Text;
+            cliente.Cli_Disponible = true;
+        }
+
+        /// <summary>
+        /// Alta de cliente
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnAlta_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (validarCampos() == true)
+            {
+                MessageBoxResult resultado = MessageBox.Show("Los siguientes datos son correctos? " + txtDni.Text + ", " + txtNombre.Text + ", " +
+                             txtApellido.Text + ", " + txtTelefono.Text + ", " + txtEmail.Text, "Atención", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (resultado == MessageBoxResult.Yes)
+                {
+                    MessageBox.Show("Cliente Guardado con exito");
+                    Cliente cliente = new Cliente(Int32.Parse(txtDni.Text), txtNombre.Text, txtApellido.Text, txtTelefono.Text, txtEmail.Text);
+                }
+                limpiarCampos();
+            }
+            else
+            {
+                MessageBoxResult resultado = MessageBox.Show("Formulario incompleto ", "Atención");
+            }
+
+        }
+
+        /// <summary>
+        /// Limpia los campos
+        /// </summary>
+        private void limpiarCampos()
+        {
+            txtDni.Text = "";
+            txtNombre.Text = "";
+            txtApellido.Text = "";
+            txtTelefono.Text = "";
+            txtEmail.Text = "";
+        }
+
+        /// <summary>
+        /// Validaciones
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// 
+        private bool validarCampos()
+        {
+            if (txtDni.Text == "" || txtNombre.Text == "" || txtApellido.Text == "" || txtTelefono.Text == "" || txtEmail.Text == "")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            int result;
+
+            if (!(int.TryParse(e.Text, out result) || e.Text == "."))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void PreviewTextInputOnlyLetters(object sender, TextCompositionEventArgs e)
+        {
+            int character = Convert.ToInt32(Convert.ToChar(e.Text));
+            if ((character >= 65 && character <= 90) || (character >= 97 && character <= 122))
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+
     }
 }
--- Vistas/WPFPelicula.xaml.cs
+++ Vistas/WPFPelicula.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
+﻿using ClasesBase;
+using Microsoft.Win32;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Vistas
 {
@@ -20,9 +13,98 @@ namespace Vistas
     /// </summary>
     public partial class WPFPelicula : UserControl
     {
+        Clasificacion clasificacion = new Clasificacion();
+        Genero genero = new Genero();
+        ArrayList clasificaciones = new ArrayList();
+        List<Genero> generos = new List<Genero>();
+
         public WPFPelicula()
         {
             InitializeComponent();
+            crearClasificaciones();
+            cargarComboClasificacion();
+            crearGeneros();
+            cargarComboGenero();
+        }
+
+        private void crearGeneros()
+        {
+            generos.Add(new Genero(0, "Comedia"));
+            generos.Add(new Genero(1, "Terror"));
+            generos.Add(new Genero(2, "Ciencia Ficcion"));
+            generos.Add(new Genero(3, "Musicales"));
+        }
+
+        private void cargarComboGenero()
+        {
+            cmbGenero.ItemsSource = generos;
+            cmbGenero.DisplayMemberPath = "Gnr_Descripcion";
+            cmbGenero.SelectedValue = "Gnr_Id";
+            cmbGenero.SelectedIndex = 0;
+        }
+
+        private void crearClasificaciones()
+        {
+            clasificaciones.Add(new Clasificacion(0, "A"));
+            clasificaciones.Add(new Clasificacion(1, "B"));
+            clasificaciones.Add(new Clasificacion(2, "B15"));
+            clasificaciones.Add(new Clasificacion(3, "C"));
+        }
+
+        private void cargarComboClasificacion()
+        {
+            cmbClasificacion.ItemsSource = clasificaciones;
+            cmbClasificacion.DisplayMemberPath = "Cls_Descripcion";
+            cmbClasificacion.SelectedValue = "Cls_Id";
+            cmbClasificacion.SelectedIndex = 0;
+        }
+
+        private void BtnAltaPelicula_Click(object sender, RoutedEventArgs e)
+        {
+            if (validarCampos() == true)
+            {
+                MessageBoxResult resultado = MessageBox.Show("Los siguientes datos son correctos? " + txtTitulo.Text + ", " + txtDuracion.Text + ", " +
+                Int32.Parse(cmbGenero.SelectedIndex.ToString()) + ", " + Int32.Parse(cmbClasificacion.SelectedIndex.ToString()) + ", " + txtImagenPeli.Text, "Atención", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (resultado == MessageBoxResult.Yes)
+                {
+                    MessageBox.Show("Pelicula Guardado con exito");
+                    Pelicula nuevaPelicula = new Pelicula(0, txtTitulo.Text, txtDuracion.Text, Int32.Parse(cmbGenero.SelectedIndex.ToString()), Int32.Parse(cmbClasificacion.SelectedIndex.ToString()), txtImagenPeli.Text);
+                }
+                limpiarCampos();
+            }
+            else
+            {
+                MessageBoxResult resultado = MessageBox.Show("Formulario incompleto ", "Atención");
+            }
+
+        }
+
+        private bool validarCampos()
+        {
+            if (txtTitulo.Text == "" || txtDuracion.Text == "" || txtImagenPeli.Text == "")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void limpiarCampos()
+        {
+            txtTitulo.Text = "";
+            txtDuracion.Text = "";
+            txtImagenPeli.Text = "";
+        }
+
+        private void ExaminarImg_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.ShowDialog();
+            openFileDialog1.Filter = "Todos(*.*) | *.*| Imagenes | *.jpg; *.gif; *.png; *.bmp”";
+            openFileDialog1.DefaultExt = ".jpeg";
+            txtImagenPeli.Text = openFileDialog1.FileName;
         }
     }
 }
