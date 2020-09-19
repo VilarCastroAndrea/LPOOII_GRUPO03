a5b96d6c714a3072fba0b5f5319d830d1170ae5d
andi <andiipuckett@gmail.com>
2020-09-19 01:04:36 -0300

    Ver los altas

--- Vistas/UserControl/Proyeccion/WPFMostrarProyeccion.xaml.cs
+++ Vistas/UserControl/Proyeccion/WPFMostrarProyeccion.xaml.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Vistas.UserControl.Proyeccion
+﻿namespace Vistas.UserControl.Proyeccion
 {
     /// <summary>
     /// Lógica de interacción para WPFMostrarProyeccion.xaml
     /// </summary>
-    public partial class WPFMostrarProyeccion : UserControl
+    public partial class WPFMostrarProyeccion
     {
         public WPFMostrarProyeccion()
         {
