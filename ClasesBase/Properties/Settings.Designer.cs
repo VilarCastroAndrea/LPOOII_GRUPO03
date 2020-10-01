e1d1617a40194202327182f4d2ec2e1cae73831c
Alejandro <reynaga2807@gmail.com>
2020-10-01 16:00:58 -0300

    punto2 terminado sin imagen solo consigna del tp

--- ClasesBase/Properties/Settings.Designer.cs
+++ ClasesBase/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace ClasesBase.Properties {
                 return ((string)(this["cinesConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\men_v\\Desktop\\facult" +
+            "ad\\LPOOII\\BD\\cines.mdf;Integrated Security=True;Connect Timeout=30")]
+        public string cinesConnectionString1 {
+            get {
+                return ((string)(this["cinesConnectionString1"]));
+            }
+        }
     }
 }
---
+++ ClasesBase/TrabajarPeliculas.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace ClasesBase
+{
+    public class TrabajarPeliculas
+    {
+
+        public TrabajarPeliculas() { }
+
+        public DataTable traerPeliculas()
+        {
+            SqlConnection conn = new SqlConnection(Properties.Settings.Default.cinesConnectionString1);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT * FROM Pelicula";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = conn;
+
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            return (dt);
+        }
+    }
+}
