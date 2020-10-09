3eaa80dee8e7f2ac766c6630631e6d6ef2c24f99
Lucho <Lucho@DESKTOP-CNTMO14>
2020-10-09 17:16:11 -0300

    boton rojo

--- ClasesBase/Cliente.cs
+++ ClasesBase/Cliente.cs
@@ -1,6 +1,7 @@
-﻿namespace ClasesBase
+﻿using System.ComponentModel;
+namespace ClasesBase
 {
-    public class Cliente
+    public class Cliente : IDataErrorInfo
     {
         private int cli_DNI;
         private string cli_Nombre;
@@ -49,6 +50,7 @@
             this.cli_Email = cli_Email;
             this.cli_Disponible = cli_Disponible;
         }
+        
 
         /// <summary>
         /// Get & Set
@@ -59,5 +61,70 @@
         public string Cli_Telefono { get => cli_Telefono; set => cli_Telefono = value; }
         public string Cli_Email { get => cli_Email; set => cli_Email = value; }
         public bool Cli_Disponible { get => cli_Disponible; set => cli_Disponible = value; }
+
+        public string Error => throw new System.NotImplementedException();
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string msg_error = null;
+
+                switch (columnName)
+                {
+                    case "Cli_DNI":
+                        msg_error = validar_Dni();
+                        break;
+                    case "Cli_Nombre":
+                        msg_error = validar_Nombre();
+                        break;
+                    case "Cli_Apellido":
+                        msg_error = validar_Apellido();
+                        break;
+                    case "Cli_Telefono":
+                        msg_error = validar_Telefono();
+                        break;
+                }
+                return msg_error;
+            }
+        }
+
+        private string validar_Dni()
+        {
+            
+            if (string.IsNullOrEmpty(Cli_DNI.ToString()))
+            {
+                return "El valor del campo es obligatorio";
+            }
+            return null;
+        }
+
+        private string validar_Nombre()
+        {
+            if (string.IsNullOrEmpty(Cli_Nombre))
+            {
+                return "El valor del campo es obliatorio";
+            }
+            return null;
+        }
+
+        private string validar_Apellido()
+        {
+            if (string.IsNullOrEmpty(Cli_Apellido))
+            {
+                return "El valor del campo es obliatorio";
+            }
+            return null;
+        }
+
+        private string validar_Telefono()
+        {
+            if (string.IsNullOrEmpty(Cli_Telefono))
+            {
+                return "El valor del campo es obliatorio";
+            }
+            return null;
+        }
+
     }
 }
---
+++ ClasesBase/TrabajarCliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesBase
+{
+    public class TrabajarCliente
+    {
+        public Cliente TraerCliente()
+        {
+            Cliente oCliente = new Cliente();
+            oCliente.Cli_DNI = 0;
+            oCliente.Cli_Nombre = "";
+            oCliente.Cli_Apellido = "";
+            oCliente.Cli_Telefono = "";
+            return oCliente;
+        }
+    }
+}
