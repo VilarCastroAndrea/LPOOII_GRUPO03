ba39fcab51077c4c27711b3108935b2e10d4909f
josue487 <josue487@gmail.com>
2020-10-15 08:59:31 -0300

    trabajar Roles

---
+++ ClasesBase/TrabajarRol.cs
@@ -0,0 +1,119 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace ClasesBase
+{
+    class TrabajarRol
+    {
+        /// <summary>
+        /// Alta Rol con stored procedure
+        /// </summary>
+        /// <param name="rol"></param>
+        public static void altaRol(Rol rol)
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "altaRol";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cnn;
+            cmd.Parameters.AddWithValue("@descripcion", rol.Rol_Descripcion);
+            cmd.Parameters.AddWithValue("@disponible", true);
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        /// <summary>
+        /// Baja de rol logica con stored procedure
+        /// </summary>
+        /// <param name="codigoRol"></param>
+        /// <param name="disponible"></param>
+        public static void bajaUsuario(int codigoRol, bool disponible)
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "bajaRol";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cnn;
+            cmd.Parameters.AddWithValue("@codigoRol", codigoRol);
+            cmd.Parameters.AddWithValue("@disponible", disponible);
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+
+
+        /// <summary>
+        /// Baja de rol fisica con stored procedure
+        /// </summary>
+        /// <param name="codigoRol"></param>
+        public static void bajaRolFisica(int codigoRol)
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "bajaRolFisica";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cnn;
+            cmd.Parameters.AddWithValue("@rolCodigo", codigoRol);
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+
+        }
+
+
+        /// <summary>
+        /// Lista todos los roles
+        /// </summary>
+        /// <returns></returns>
+        public static DataTable listarRoles()
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "listarRoles";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cnn;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        /// <summary>
+        /// Listar Roles disponibles
+        /// </summary>
+        /// <returns></returns>
+        public static DataTable listarButacasDisponibles()
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "listarRolDisponnible";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cnn;
+            cmd.Parameters.AddWithValue("@disponible", true);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        /// <summary>
+        /// Modificar Rol con stored procedure
+        /// </summary>
+        /// <param name="rol"></param>
+        public static void modificarButaca(Rol rol)
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "modificarButaca";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cnn;
+            cmd.Parameters.AddWithValue("@codigo", rol.Rol_Codigo);
+            cmd.Parameters.AddWithValue("@descripcion", rol.Rol_Descripcion);
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+    }
+}
