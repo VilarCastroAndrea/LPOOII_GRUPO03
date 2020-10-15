7f0833c5fba5bae3e0b948329f94228f588678e0
andrea castro <andiipuckett@gmail.com>
2020-10-15 01:53:01 -0300

    Trabajar usuario

---
+++ ClasesBase/TrabajarUsuario.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClasesBase
+{
+    public class TrabajarUsuario
+    {
+        /// <summary>
+        /// Lista de usuarios
+        /// </summary>
+        /// <returns></returns>
+        public static DataTable listarUsuario()
+        {
+            SqlConnection conn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "listarUsuarioss";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = conn;
+
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            da.Fill(dt);
+
+            return (dt);
+        }
+
+        /// <summary>
+        /// Listar usuario disponibles
+        /// </summary>
+        /// <returns></returns>
+        public static DataTable listaUsuarioDisponible()
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "listarUsuarioDisponible";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cnn;
+            cmd.Parameters.AddWithValue("@disponible", true);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        /// <summary>
+        /// Alta usuario en la base de datos.
+        /// </summary>
+        public static void altaUsuario(Usuario usuario)
+        {
+            SqlConnection conn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "altaUsuario";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = conn;
+
+            cmd.Parameters.AddWithValue("@nombreUsuario", usuario.Usu_NombreUsuario);
+            cmd.Parameters.AddWithValue("@password", usuario.Usu_Password);
+            cmd.Parameters.AddWithValue("@apellidoNombre", usuario.Usu_ApellidoNombre);
+            cmd.Parameters.AddWithValue("@codigo", usuario.Rol_Codigo);
+            cmd.Parameters.AddWithValue("@disponible", usuario.Usu_Disponible);
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        /// <summary>
+        /// Baja de usuario con stored procedure
+        /// </summary>
+        public static void bajaUsuario(int codigo, bool disponible)
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "bajaUsuario";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cnn;
+            cmd.Parameters.AddWithValue("@codigoId", codigo);
+            cmd.Parameters.AddWithValue("@disponible", disponible);
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        /// <summary>
+        /// Baja de usuario fisica con stored procedure
+        /// </summary>
+        public static void bajaUsuarioFisica(int codigo)
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "bajaUsuarioFisica";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cnn;
+            cmd.Parameters.AddWithValue("@usuarioId", codigo);
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        /// <summary>
+        /// Modificar usuario con stored procedure
+        /// </summary>
+        public static void modificarUsaurio(Usuario usuario)
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "modificarUsuario";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cnn;
+            cmd.Parameters.AddWithValue("@id", usuario.Usu_Id);
+            cmd.Parameters.AddWithValue("@nombreUsuario", usuario.Usu_NombreUsuario);
+            cmd.Parameters.AddWithValue("@password", usuario.Usu_Password);
+            cmd.Parameters.AddWithValue("@apellidoNombre", usuario.Usu_ApellidoNombre);
+            cmd.Parameters.AddWithValue("@codigoRol", usuario.Rol_Codigo);
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+    }
+}
