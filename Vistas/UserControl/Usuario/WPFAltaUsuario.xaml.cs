6593e05120ba8d95de5ae4a4327b33f0184b0061
andrea castro <andiipuckett@gmail.com>
2020-10-15 00:55:59 -0300

    Alta Usuario

---
+++ Vistas/UserControl/Usuario/WPFAltaUsuario.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vistas.UserControl.Usuario
+{
+    /// <summary>
+    /// Lógica de interacción para WPFAltaUsuario.xaml
+    /// </summary>
+    public partial class WPFAltaUsuario : UserControl
+    {
+        public WPFAltaUsuario()
+        {
+            InitializeComponent();
+        }
+    }
+}
