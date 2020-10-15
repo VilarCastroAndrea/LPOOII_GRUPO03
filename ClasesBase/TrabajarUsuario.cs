4def2bbabd039928d2d70827e7f75bae118c9297
Gustavo Ordoñez <enter_the_matrix_10@hotmail.com>
2020-10-15 10:53:27 -0300

    Agregados metodos para modificar y eliminar usuario en trabajarUsuario

--- ClasesBase/TrabajarUsuario.cs
+++ ClasesBase/TrabajarUsuario.cs
@@ -11,6 +11,10 @@ namespace ClasesBase
 {
     public class TrabajarUsuario
     {
+        /// <summary>
+        /// Obtie los usuarios de la base de datos
+        /// </summary>
+        /// <returns></returns>
         public ObservableCollection<Usuario> TraerUsuarios()
         {
             ObservableCollection<Usuario> listaUsuarios = new ObservableCollection<Usuario>();
@@ -21,6 +25,10 @@ namespace ClasesBase
             return listaUsuarios;
         }
 
+        /// <summary>
+        /// Inserta un usuario en la base de datos
+        /// </summary>
+        /// <param name="usu"></param>
         public void InsertarUsuario(Usuario usu)
         {
             SqlConnection conn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
@@ -38,6 +46,51 @@ namespace ClasesBase
             cmd.Parameters.AddWithValue("@rolCod", usu.Rol_Codigo);
             cmd.Parameters.AddWithValue("@disponible", usu.Usu_Disponible);
 
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        /// <summary>
+        /// Elimina un usuario de la base de datos
+        /// </summary>
+        /// <param name="usu"></param>
+        public void EliminarUsuario(Usuario usu)
+        {
+            SqlConnection conn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "DELETE FROM Usuario WHERE CLI_ID = @id";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = conn;
+
+            cmd.Parameters.AddWithValue("@id", usu.Usu_Id);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        /// <summary>
+        /// Actualiza un usuario de la base de datos
+        /// </summary>
+        /// <param name="usu"></param>
+        public void ActualizarUsuario(Usuario usu)
+        {
+            SqlConnection conn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "UPDATE Usuario set " +
+                              "USU_NombreUsuario=@nombUsu, USU_Password=@pass," +
+                              " USU_ApellidoNombre=@apNomb, ROL_Codigo=@rol, " +
+                              "USU_Disponible=@disp " +
+                              "WHERE USU_ID=@id";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = conn;
+
+            cmd.Parameters.AddWithValue("@id", usu.Usu_Id);
+            cmd.Parameters.AddWithValue("@nombUsu", usu.Usu_NombreUsuario);
+            cmd.Parameters.AddWithValue("@pass", usu.Usu_Password);
+            cmd.Parameters.AddWithValue("@apNomb", usu.Usu_ApellidoNombre);
+            cmd.Parameters.AddWithValue("@rol", usu.Rol_Codigo);
+            cmd.Parameters.AddWithValue("@disp", usu.Usu_Disponible);
             conn.Open();
             cmd.ExecuteNonQuery();
             conn.Close();
