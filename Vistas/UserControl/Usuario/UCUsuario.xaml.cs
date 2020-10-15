4e482d67165d9f6507ece05f479986045c1a84c2
Gustavo Ordoñez <enter_the_matrix_10@hotmail.com>
2020-10-15 18:48:03 -0300

    Acomodado un poco el controlador de usuario

--- Vistas/UserControl/Usuario/UCUsuario.xaml.cs
+++ Vistas/UserControl/Usuario/UCUsuario.xaml.cs
@@ -56,6 +56,10 @@ namespace Vistas.UserControl.Usuario
             btnSiguiente.Visibility = Visibility.Visible;
             btnUltimo.Visibility = Visibility.Visible;
             btnCancelar.Visibility = Visibility.Hidden;
+            btnModificarUsuario.Visibility = Visibility.Visible;
+            //Cambiar los textbox
+            stpFormulario.Visibility = Visibility.Hidden;
+            stpContent.Visibility = Visibility.Visible;
         }
 
         /// <summary>
@@ -65,14 +69,14 @@ namespace Vistas.UserControl.Usuario
         private Usuario CapturarInputs()
         {
             Usuario oUsuario = new Usuario();
-            if (!String.IsNullOrEmpty(txtCodigo.Text))
-            {
-                oUsuario.Usu_Id = int.Parse(txtCodigo.Text);
-            }
-            oUsuario.Usu_NombreUsuario = txtUserName.Text;
-            oUsuario.Usu_Password = txtPassword.Text;
-            oUsuario.Usu_ApellidoNombre = txtApellido.Text + "," + txtNombre.Text;
-            oUsuario.Rol_Codigo = int.Parse(txtRol.Text);
+            //if (!String.IsNullOrEmpty(txtCodigo.Text))
+            //{
+            //    oUsuario.Usu_Id = int.Parse(txtCodigo.Text);
+            //}
+            oUsuario.Usu_NombreUsuario = txtUserNameFrm.Text;
+            oUsuario.Usu_Password = txtPasswordFrm.Text;
+            oUsuario.Usu_ApellidoNombre = txtApellidoFrm.Text + "," + txtNombreFrm.Text;
+            oUsuario.Rol_Codigo = int.Parse(txtRolFrm.Text);
             return oUsuario;
         }
 
@@ -82,16 +86,18 @@ namespace Vistas.UserControl.Usuario
         /// </summary>
         private void LimpiarCampos()
         {
-            txtCodigo.Text = "";
-            txtApellido.Text = "";
-            txtNombre.Text = "";
-            txtPassword.Text = "";
-            txtRol.Text = "";
-            txtUserName.Text = "";
+            txtApellidoFrm.Text = "";
+            txtNombreFrm.Text = "";
+            txtPasswordFrm.Text = "";
+            txtRolFrm.Text = "";
+            txtUserNameFrm.Text = "";
         }
 
         private void BtnNuevoUsuario_Click(object sender, RoutedEventArgs e)
         {
+            //Prepara los botones de la vista
+            stpContent.Visibility = Visibility.Hidden;
+            stpFormulario.Visibility = Visibility.Visible;
             btnEliminarUsuario.Visibility = Visibility.Hidden;
             btnPrimero.Visibility = Visibility.Hidden;
             btnPrevio.Visibility = Visibility.Hidden;
@@ -131,12 +137,93 @@ namespace Vistas.UserControl.Usuario
 
         private void BtnCancelar_Click(object sender, RoutedEventArgs e)
         {
+            //Prepara la visibilidad de los botones
+            //Botones de operaciones
+            stpContent.Visibility = Visibility.Visible;
+            stpFormulario.Visibility = Visibility.Hidden;
             btnEliminarUsuario.Visibility = Visibility.Visible;
+            btnCancelar.Visibility = Visibility.Hidden;
+            btnGuardarModificacion.Visibility = Visibility.Hidden;
+            btnGuardarUsuario.Visibility = Visibility.Hidden;
+
+            //Botones de navegacion
+            HabilitarNavegacion();
+            
+        }
+
+        private void BtnModificarUsuario_Click(object sender, RoutedEventArgs e)
+        {
+            //Asigna los valores del usuario existente en un formulario para editar
+            txtApellidoFrm.Text = txtApellido.Text;
+            txtNombreFrm.Text = txtNombre.Text;
+            txtPasswordFrm.Text = txtPassword.Text;
+            txtRolFrm.Text = txtRol.Text;
+            txtUserNameFrm.Text = txtUserName.Text;
+
+            //Prepara los botones de la vista
+            //Botones de Operaciones
+            btnCancelar.Visibility = Visibility.Visible;
+            btnNuevoUsuario.Visibility = Visibility.Hidden;
+            btnGuardarUsuario.Visibility = Visibility.Hidden;
+            btnModificarUsuario.Visibility = Visibility.Hidden;
+            btnGuardarModificacion.Visibility = Visibility.Visible;
+            btnEliminarUsuario.Visibility = Visibility.Hidden;
+
+            //Botones de Navegación
+            DeshabilitarNavegacion();
+            
+        }
+
+        private void BtnGuardarModificacion_Click(object sender, RoutedEventArgs e)
+        {
+            //Capturar los valores del textbox
+            Usuario oUsu = CapturarInputs();
+            oUsu.Usu_Disponible = true;
+            //Guardar Usuario en la bd
+            TrabajarUsuario.ActualizarUsuario(oUsu);
+            //Actualizar la lista
+
+            //Mostrar mensaje de exito
+            MessageBox.Show("Usuario actualizado correctamente.");
+
+            //Habilitar botones
+            //Botones de Operaciones
+            btnCancelar.Visibility = Visibility.Hidden;
+            btnModificarUsuario.Visibility = Visibility.Visible;
+            btnGuardarModificacion.Visibility = Visibility.Hidden;
+            btnEliminarUsuario.Visibility = Visibility.Visible;
+
+            //Botones de Navegación
+            HabilitarNavegacion();
+
+            //Cambiar los textbox
+            stpFormulario.Visibility = Visibility.Hidden;
+            stpContent.Visibility = Visibility.Visible;
+        }
+
+        /// <summary>
+        /// Habilita los botones de navegación de la vista
+        /// </summary>
+        private void HabilitarNavegacion()
+        {
+            //Botones de Navegación
             btnPrimero.Visibility = Visibility.Visible;
             btnPrevio.Visibility = Visibility.Visible;
             btnSiguiente.Visibility = Visibility.Visible;
             btnUltimo.Visibility = Visibility.Visible;
-            btnCancelar.Visibility = Visibility.Hidden;
+        }
+
+
+        /// <summary>
+        /// Deshabilita los botones de navegación de la vista
+        /// </summary>
+        private void DeshabilitarNavegacion()
+        {
+            //Botones de Navegación
+            btnPrimero.Visibility = Visibility.Hidden;
+            btnPrevio.Visibility = Visibility.Hidden;
+            btnSiguiente.Visibility = Visibility.Hidden;
+            btnUltimo.Visibility = Visibility.Hidden;
         }
     }
 }
