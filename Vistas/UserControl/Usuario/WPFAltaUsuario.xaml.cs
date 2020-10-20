4638331e30622bc2b9a36fd3bf9f4a9478803deb
sebamiranda <56943363+sebamiranda@users.noreply.github.com>
2020-10-19 23:25:33 -0300

    b

--- Vistas/UserControl/Usuario/WPFAltaUsuario.xaml.cs
+++ Vistas/UserControl/Usuario/WPFAltaUsuario.xaml.cs
@@ -12,17 +12,44 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ClasesBase;
 
 namespace Vistas.UserControl.Usuario
 {
     /// <summary>
     /// Lógica de interacción para WPFAltaUsuario.xaml
     /// </summary>
-    public partial class WPFAltaUsuario 
+    public partial class WPFAltaUsuario
     {
         public WPFAltaUsuario()
         {
             InitializeComponent();
+            cargarComboRol();
         }
+
+        private void BtnAltaUsuario_Click(object sender, RoutedEventArgs e)
+        {
+            ClasesBase.Usuario usuario = new ClasesBase.Usuario();
+
+
+
+            usuario.Usu_ApellidoNombre = txtApellidoNombre.Text;
+            usuario.Usu_NombreUsuario = txtNombreUsuario.Text;
+            usuario.Usu_Password = txtPassword.Text;
+            usuario.Usu_Disponible = true;
+            usuario.Rol_Codigo = 1;
+
+            TrabajarUsuario.altaUsuario(usuario);
+
+            MessageBox.Show("Usuario agregado correctamente");
+        }
+        private void cargarComboRol()
+        {
+            cmbRol.ItemsSource = TrabajarRol.listarRoles();
+            cmbRol.SelectedValuePath = "Rol_Codigo";
+            cmbRol.DisplayMemberPath = "Rol_Descripcion";
+        }
+
+
     }
 }
