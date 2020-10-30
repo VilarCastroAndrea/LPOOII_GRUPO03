c4698b3f7cd1d42029abf9b82b07f51a18398bf8
josue487 <josue487@gmail.com>
2020-10-29 21:56:18 -0300

    Buscar y listar Peliculas

--- ClasesBase/Trabajar/TrabajarPelicula.cs
+++ ClasesBase/Trabajar/TrabajarPelicula.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
+using System.Collections.ObjectModel;
 
 namespace ClasesBase
 {
@@ -44,6 +45,42 @@ namespace ClasesBase
             cmd.ExecuteNonQuery();
             cnn.Close();
         }
+
+
+        /// <summary>
+        /// Lista todas las peliculas
+        /// </summary>
+        /// <returns></returns>
+        public static ObservableCollection<Pelicula> traerPeliculas()
+        {
+            ObservableCollection<Pelicula> coleccionDePeliculas = new ObservableCollection<Pelicula>();
+            Pelicula pelicula = null;
+
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
+            cnn.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "ListarPeliculas";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cnn;
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                pelicula = new Pelicula();
+                pelicula.Peli_Clasificacion = (string)reader["Clasificacion"];
+                pelicula.Peli_Codigo = (int)reader["Codigo"];
+                pelicula.Peli_Disponible = (bool)reader["Disponible"];
+                pelicula.Peli_Duracion = (string)reader["Duracion"];
+                pelicula.Peli_Genero = (string)reader["Genero"];
+                pelicula.Peli_Titulo = (string)reader["Titulo"];
+                coleccionDePeliculas.Add(pelicula);
+            }
+            cnn.Close();
+            return coleccionDePeliculas;
+        }
+
+
 
 
 
@@ -131,19 +168,37 @@ namespace ClasesBase
         /// </summary>
         /// <param name="sPattern"></param>
         /// <returns></returns>
-        public static DataTable buscarPelicula(string sPattern)
+        public static ObservableCollection<Pelicula> buscarPelicula(string sPattern)
         {
+
+            ObservableCollection<Pelicula> coleccionDePelicula = new ObservableCollection<Pelicula>();
+            Pelicula pelicula = null;
+
             SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.cinesConnectionString);
+            cnn.Open();
+
             SqlCommand cmd = new SqlCommand();
             cmd.CommandText = "buscarPelicula";
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Connection = cnn;
-            cmd.Parameters.AddWithValue("@titulo", "%" + sPattern + "%");
-            cmd.Parameters.AddWithValue("@clasificacion", "%" + sPattern + "%");
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            da.Fill(dt);
-            return dt;
+            cmd.Parameters.AddWithValue("@id", sPattern);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                pelicula = new Pelicula();
+                pelicula.Peli_Clasificacion = (string)reader["Clasificacion"];
+                pelicula.Peli_Codigo = (int)reader["Codigo"];
+                pelicula.Peli_Disponible = (bool)reader["Disponible"];
+                pelicula.Peli_Duracion = (string)reader["Duracion"];
+                pelicula.Peli_Genero = (string)reader["Genero"];
+                pelicula.Peli_Titulo = (string)reader["Titulo"];
+                coleccionDePelicula.Add(pelicula);
+            }
+            cnn.Close();
+            return coleccionDePelicula;
+
+
         }
 
 
