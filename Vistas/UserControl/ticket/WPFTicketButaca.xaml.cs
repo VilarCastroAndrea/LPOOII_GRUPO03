7ca6bec3d65b5c68d17d210412eeee1c0caf0d26
andrea castro <andiipuckett@gmail.com>
2020-11-07 19:17:25 -0300

    alta ticket

---
+++ Vistas/UserControl/ticket/WPFTicketButaca.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClasesBase;
+namespace Vistas.UserControl.ticket
+{
+    /// <summary>
+    /// Lógica de interacción para WPFTicketButaca.xaml
+    /// </summary>
+    public partial class WPFTicketButaca 
+    {
+        Ticket ticket1 = new Ticket(); 
+        public WPFTicketButaca(Ticket ticket)
+        {
+            InitializeComponent();
+            ticket1 = ticket;
+            txtProyeccion.Text = Convert.ToString(ticket.Proy_Codigo);
+            txtCliente.Text = Convert.ToString(ticket.Cli_DNI);
+        }
+
+        private void BtnConfirmar_Click(object sender, RoutedEventArgs e)
+        {
+            WPFTicketImpresion impresion = new WPFTicketImpresion(ticket1);
+            impresion.Show();
+        }
+    }
+}
---
+++ Vistas/UserControl/ticket/WPFTicketCliente.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vistas.UserControl.ticket
+{
+    /// <summary>
+    /// Lógica de interacción para WPFTicketCliente.xaml
+    /// </summary>
+    public partial class WPFTicketCliente 
+    {
+        public WPFTicketCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void Clientes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataRowView item = (DataRowView)listaClienteTicket.SelectedItem;
+            ClasesBase.Cliente cliente = new ClasesBase.Cliente();
+            txtDNI.Text = Convert.ToString(item["DNI"]);
+            txtApellido.Text = Convert.ToString(item["Apellido"]);
+            txtNombre.Text = Convert.ToString(item["Nombre"]);
+            WPFTicket ticketPadre = new WPFTicket();
+            ticketPadre.cargarCliente(Convert.ToInt32(item["DNI"]));
+        }
+    }
+}
