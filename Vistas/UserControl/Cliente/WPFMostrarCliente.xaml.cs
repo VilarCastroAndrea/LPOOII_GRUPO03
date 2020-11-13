ef46cda9297484765a5e8a07cb983fce8002902c
sebamiranda <56943363+sebamiranda@users.noreply.github.com>
2020-11-12 21:03:10 -0300

    3

--- Vistas/UserControl/Cliente/WPFMostrarCliente.xaml.cs
+++ Vistas/UserControl/Cliente/WPFMostrarCliente.xaml.cs
@@ -9,6 +9,7 @@ namespace Vistas
     /// </summary>
     public partial class WPFMostrarCliente
     {
+        WPFCliente proyeccionPadre;
         /// <summary>
         /// Cliente para realizar el alta
         /// </summary>
@@ -19,11 +20,11 @@ namespace Vistas
         /// </summary>
         private WPFCliente oPadre;
 
-        public WPFMostrarCliente()
+        public WPFMostrarCliente(WPFCliente padre)
         {
-            cliente = new Cliente();
             InitializeComponent();
-
+            cliente = new Cliente();
+            proyeccionPadre = padre;
         }
 
         public WPFMostrarCliente(Cliente verCliente, WPFCliente padre)
@@ -54,21 +55,34 @@ namespace Vistas
 
         private void BtnModificar_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            Cliente cli = new Cliente();
-            cli.Cli_DNI = int.Parse(txtDni.Text);
-            cli.Cli_Apellido = txtApellido.Text;
-            cli.Cli_Nombre = txtNombre.Text;
-            cli.Cli_Telefono = txtTelefono.Text;
-            cli.Cli_Email = txtEmail.Text;
-            cli.Cli_Disponible = true;
+            if (validarCampos() == true)
+            {
 
-            TrabajarClientes.ActualizarCliente(cli);
+                ClasesBase.Cliente cliente = new ClasesBase.Cliente();
 
-            MessageBox.Show("Se modifico el registro");
-            //Se debe mostrar el formulario de alta cliente para que se actualize el grid en caso contrario sale error
-            oPadre.ActualizarDataGrid();
-            limpiarcampos();
 
+                Cliente cli = new Cliente();
+                cli.Cli_DNI = int.Parse(txtDni.Text);
+                cli.Cli_Apellido = txtApellido.Text;
+                cli.Cli_Nombre = txtNombre.Text;
+                cli.Cli_Telefono = txtTelefono.Text;
+                cli.Cli_Email = txtEmail.Text;
+                cli.Cli_Disponible = true;
+                proyeccionPadre.modificarCliente(cliente);
+                MessageBoxResult resultado = MessageBox.Show("Se modifico la pelicula con exito", "Atención");
+                limpiarcampos();
+            }
+            else
+            {
+                MessageBoxResult resultado = MessageBox.Show("Formulario incompleto ", "Atención");
+            }
+            //TrabajarClientes.ActualizarCliente(cli);
+
+            //    MessageBox.Show("Se modifico el registro");
+            //    //Se debe mostrar el formulario de alta cliente para que se actualize el grid en caso contrario sale error
+            //    oPadre.ActualizarDataGrid();
+            //    limpiarcampos();
+            //}
         }
 
         private void BtnBaja_Click(object sender, System.Windows.RoutedEventArgs e)
@@ -98,8 +112,19 @@ namespace Vistas
             }
         }
 
+            private bool validarCampos()
+            {
+                if (txtDni.Text == "" || txtApellido.Text == "" || txtNombre.Text == "" || txtTelefono.Text == "" || txtEmail.Text == "")
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
 
-        private void limpiarcampos()
+            private void limpiarcampos()
         {
             txtDni.Text = null;
             txtApellido.Text = null;
